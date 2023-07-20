b61ce5b5b8e7ae0e0ec2f39be11262f3fd33cdeb
kavi-ivykids <136069340+kavi-ivykids@users.noreply.github.com>
2023-07-20 23:25:13 +0530

    added the exports of the generated files

--- proto/lib/proto.dart
+++ proto/lib/proto.dart
@@ -3,5 +3,9 @@
 /// More dartdocs go here.
 library;
 
+export 'src/generated/todo.pb.dart';
+export 'src/generated/todo.pbenum.dart';
+export 'src/generated/todo.pbjson.dart';
+
 
 // TODO: Export any libraries intended for clients of this package.
