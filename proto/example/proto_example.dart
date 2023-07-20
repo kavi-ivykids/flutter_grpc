383e0e4843db718407ac4fdc6b376475607da685
kavi-ivykids <136069340+kavi-ivykids@users.noreply.github.com>
2023-07-20 23:20:58 +0530

    added generated pb files

--- proto/example/proto_example.dart
+++ proto/example/proto_example.dart
@@ -1,6 +1,1 @@
-import 'package:proto/proto.dart';
-
-void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
-}
+void main() {}
--- proto/lib/proto.dart
+++ proto/lib/proto.dart
@@ -3,6 +3,5 @@
 /// More dartdocs go here.
 library;
 
-export 'src/proto_base.dart';
 
 // TODO: Export any libraries intended for clients of this package.
